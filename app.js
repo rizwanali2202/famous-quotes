d4c17e740d4c16ecfca90d74f5bb0277a512012d
Rizwan Ali <164035652+rizwanali2202@users.noreply.github.com>
2024-03-25 18:07:30 +0500

    famous Quotes

---
+++ app.js
@@ -0,0 +1,3 @@
+var famousPerson = "Quaid Azam";
+var quotes = "work work and always work";
+console.log("".concat(famousPerson, ", \"said\" ").concat(quotes));
---
+++ app.ts
@@ -0,0 +1,3 @@
+let famousPerson : string = "Quaid Azam";
+let quotes : string = "work work and always work";
+console.log(`${famousPerson}, said ${quotes}`);
